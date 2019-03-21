760b050435d7c29a11b358c2792c71bac2cfab59
Gabriel Ribeiro <>
2019-03-21 10:03:28 -0300

    New methods

--- src/ios/KeyboardAppearance.m
+++ src/ios/KeyboardAppearance.m
@@ -4,29 +4,76 @@
 
 @interface KeyboardAppearance : CDVPlugin {
   // Member variables go here.
+  @protected NSString* _keyboardStyle;
 }
 
-- (void)setKeyboardAppearance:(CDVInvokedUrlCommand*)command;
+@property (readwrite, assign) NSString* keyboardStyle;
+
+- (void)keyboardStyle:(CDVInvokedUrlCommand*)command;
+
 @end
 
 @implementation KeyboardAppearance
 
-- (void)setKeyboardAppearance:(CDVInvokedUrlCommand*)command
-{
-    CDVPluginResult* pluginResult = nil;
-    NSString* param = [command.arguments objectAtIndex:0];
+#pragma mark Initialize
 
-    if ([param isEqualToString:@"dark"]) {
-        [[UITextField appearance] setKeyboardAppearance:UIKeyboardAppearanceDark];
-    } else if ([param isEqualToString:@"light"]) {
-        [[UITextField appearance] setKeyboardAppearance:UIKeyboardAppearanceLight];
-    } else {
-        [[UITextField appearance] setKeyboardAppearance:UIKeyboardAppearanceDefault];
+static NSString* UIClassString;
+static NSString* WKClassString;
+static NSString* UITraitsClassString;
+
+- (void)pluginInitialize
+{
+    // Create these strings at runtime so they aren't flagged
+    UIClassString = [@[@"UI", @"Web", @"Browser", @"View"] componentsJoinedByString:@""];
+    WKClassString = [@[@"WK", @"Content", @"View"] componentsJoinedByString:@""];
+    UITraitsClassString = [@[@"UI", @"Text", @"Input", @"Traits"] componentsJoinedByString:@""];
+}
+
+#pragma mark Keyboard Style
+
+- (NSString*)keyboardStyle
+{
+    return _keyboardStyle;
+}
+
+- (void)setKeyboardStyle:(NSString*)style
+{
+    if ([style isEqualToString:_keyboardStyle]) {
+        return;
     }
 
-    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:param];
+    IMP newImp = [style isEqualToString:@"dark"] ? imp_implementationWithBlock(^(id _s) {
+        return UIKeyboardAppearanceDark;
+    }) : imp_implementationWithBlock(^(id _s) {
+        return UIKeyboardAppearanceLight;
+    });
 
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    for (NSString* classString in @[UIClassString, UITraitsClassString]) {
+        Class c = NSClassFromString(classString);
+        Method m = class_getInstanceMethod(c, @selector(keyboardAppearance));
+
+        if (m != NULL) {
+            method_setImplementation(m, newImp);
+        } else {
+            class_addMethod(c, @selector(keyboardAppearance), newImp, "l@:");
+        }
+    }
+
+    _keyboardStyle = style;
+}
+
+#pragma mark Plugin interface
+
+- (void)keyboardStyle:(CDVInvokedUrlCommand*)command
+{
+    id value = [command.arguments objectAtIndex:0];
+    if ([value isKindOfClass:[NSString class]]) {
+        value = [(NSString*)value lowercaseString];
+    } else {
+        value = @"light";
+    }
+
+    self.keyboardStyle = value;
 }
 
 @end
--- www/KeyboardAppearance.js
+++ www/KeyboardAppearance.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
 exports.setKeyboardAppearance = function (arg0) {
-    exec(function () {}, function () {}, 'KeyboardAppearance', 'setKeyboardAppearance', [arg0]);
+    exec(function () {}, function () {}, 'KeyboardAppearance', 'setStyle', [arg0]);
 };
